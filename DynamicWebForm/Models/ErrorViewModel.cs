69e8b5ec0709f48ab7fc1b6fd3c36abf0a5a1ab2
Administrator <abbas@DESKTOP-82FJBE6>
2018-10-28 12:05:33 +0330

    Add project files.

---
+++ DynamicWebForm/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DynamicWebForm.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
